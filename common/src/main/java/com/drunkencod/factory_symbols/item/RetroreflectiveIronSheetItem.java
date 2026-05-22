486f6220d07a5ce140ff18032d2ef14790704efd
Sv443 <contact@sv443.net>
2026-05-22 22:28:22 +0200

    feat: retroreflective iron sheet

---
+++ common/src/main/java/com/drunkencod/factory_symbols/item/RetroreflectiveIronSheetItem.java
@@ -0,0 +1,11 @@
+package com.drunkencod.factory_symbols.item;
+
+import net.minecraft.world.item.Item;
+
+public class RetroreflectiveIronSheetItem extends Item {
+    public static final String ID = "retroreflective_iron_sheet";
+
+    public RetroreflectiveIronSheetItem(Properties properties) {
+        super(properties);
+    }
+}
